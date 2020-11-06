977035a281a694480c3d265784d23655d7fcaf04
cranberriez <jakevilevac@gmail.com>
2020-11-05 17:22:14 -0800

    replace tarkov roullette with phasmo roullette

--- main.js
+++ main.js
@@ -1,70 +1,111 @@
-var challenges = [
-    {"Steel-less Battalion": "You must play this run without armour of any form."},
-    {"Dora’s Backpack": " Your ENTIRE LOADOUT must fit within a SCAV backpack. Weapons, armour, meds/stims, ammo/mags, secure items, etc. must fit within the bag. Attachments for your chosen weapon are part of the gun and are not included in the backpack. (EDIT: The secure items must only fit in the backpack, they do not have to stay there for the raid. They can be returned to your secure case once they are accounted for. No one wants to needlessly lose a S I C C Case.)"},
-    {"Cement Shoes": " When you spawn into the raid, you must wait 5 mins before moving or preforming an action (example being taking painkillers)."},
-    {"Down to the Wire": " You must remain in the raid for the entirety of the timer. The timer must read less than 4 mins before extract can occur."},
-    {"Guns, Germs, and Steel": " The year is 1815. Your loadout may only consist of a bolt-action rifle and ammo (Guns), a sling bag, a melee weapon (Steel), one army bandage, and one injector (Germs). Rifles must be reloaded bullet-by-bullet, no additional magazines may be brought in."},
-    {"Guns, Germs, and Steel+": " Same as above, however there is to be no internal magazines. Each round must be loaded individually using the inventory screen."},
-    {"You Have Become What You Always Hated…": " Hatchling Run."},
-    {"John Pestily": " You are doing a John Wick run on Factory. Your loadout includes: An Altyn + Visor, Gzhel-K, a Five-Seven, all the injectors your body can handle. Certain elements of this loadout may vary based on individuals cash supply. Basic Formula however is: Helmet, Armour, Pistol, Injectors."},
-    {"John Pestily+": " Same as above. However, you are only allowed to use a Five-Seven and all the injectors your body can handle."},
-    {"Fire Sale": " Interchange Run."},
-    {"Back When Things Weren’t So Complicated": " You must do a Customs run; however, you may not use any part of the map that is part of “The Expansion”. You may engage into “The Expansion” but you may not go into it."},
-    {"If You’re Going to the Store…": " You must run Interchange. However, you may not leave the map without the following in your backpack: 2x Bolts of Fabric, 2x Pieces of Tech, 2x Building Supplies, 1x Tool, 1x Medical Item, 1x Knife from a dead SCAV."},
-    {"SCAVS Are Friends, Not Food": " During this raid, you may not shoot at or kill SCAVS. Raiders and SCAV Bosses are not SCAV, they are psychopaths and are not included."},
-    {"Park Ranger": " During this raid your ENTIRE LOADOUT, must only comprise of things purchased from Jaeger. Guns, Attachments, Ammo, Clothes, Bags, Meds…Everything."},
-    {"Family Picnic": " You must do a raid on Customs. However, before you can leave the map, you must go to SCAV Boat Extract campfire and consume 1x Drink, 1x Food, 1x Candy Bar. You must be sitting or prone while consuming."},
-    {"Million Dollar Man": " If you die at all, within the next three raids, you are done Tarkov for today."},
-    {"No Food After Midnight": " You may not pick-up or consume any food or beverages for the entirety of your raid."},
-    {"Magnum P.I.": " Tom Selleck may have the only mustache out there that can rival Sequisha’s. Put on a mustache, dress like it’s the 80’s, and grab yourself a Colt 1911. No bags. No armour. Only the ‘stache to protect you."},
-    {"Civilians on Patrol": " Your loadout may only be constructed using items that the AVERAGE gun owner, looking at you America, would have access to. Think basic firearms license."},
-    {"Master Roshi": " You are doing a Shoreline run. You must go to SCAV Island and drop ONE of the following items on the doorstep: a piece of food (hand-delivered) or something to play video games on (Tetriz or a G-Phone)."},
-    {"Don’t Spend It All In One Place": " Your ENTIRE LOADOUT must not exceed $70,000 Roubles. If you are using items from your stash, use their approximate retail value."},
-    {"Mom and Dad Are Fighting Again…": " You may not use any item sold by Prapor or Therapist. They aren’t talking to each other and just need to be left alone for a raid."},
-    {"Did You Change The Clocks?": " You have 10 mins until the raid ends. Once 10 mins has passed head straight for the exit."},
-    {"History of Warfare": "Fight through the ages over a multi-run adventure. Your weapons are as follows: Mosin-Nagant (1891) > PPSH (1941) > SKS (1944) > AKM (1959) > AK-74+Variants (1970) > M4A1 (1994) > MPX (2013). You can use whatever weapons you wish for this challenge, as long as there is a timeline you are following."},
-    {"Like Tears in the Rain": "You must run Shoreline at night while it’s raining. Climb to the roof of resort and leap from the roof onto the ambulance by sky bridge. Once your legs are broken and you basically dead, hunt down the android known as Sanitar. You may not use injectors or health items after leaping from the roof. This must be done within 25 mins of the raid start. Unless you spawn in the Northwest on a Tuesday after 7:00pm PST then it must be done within 20 mins."},
-    {"Who Will Save the World?": "Dress as soviets in Afghanistan: 6b43 Zabralo, unmodded ak74, tank helmet."},
-    {"Black Hawk Down": "Cosplay as delta force in Mogadishu."},
-    {"American Sniper": "Bring a sniper rifle to any map, setup in a location and stay there until you die or are killed."},
-    {"Friendly Fire": "(In a group) At the start of the raid, everyone has 15 seconds to spread out, after that you must fight until there is only 1 man left standing."},
-    {"Liftman": "Labs, camp the elevators."},
-    {"Time is Money": "Labs. Don't. Stop. Moving."},
-    {"Vacation": "Take a relaxing trip to the Shoreline resort, choose a room and relax in it until you are disturbed. Unsuppressed weapons only, and nothing covering your beautiful face."},
-]
-
-var past_three = [];
+var challenges = {
+    "EMP ": [
+        "No electronic devices",
+        "No using lights in the building",
+        "No flashlights"
+    ],
+    "Candles and Glowsticks": [
+        "Can still use all normal electronics except flashlights (including UV flashlight)"
+    ],
+    "Stubborn Cunts ”Anti vax life hax”": [
+        "No going back to truck until you want to leave",
+        "No sanity pills"
+    ],
+    "Im finna piss myself": [
+        "No crucifix",
+        "Always split up from one another"
+    ],
+    "PTFO (Play the fucking objective!!!)": [
+        "Cannot leave until ALL objectives are done",
+        "Or everyone is dead"
+    ],
+    "The NSA": [
+        "One person must remain at the truck at all times",
+        "It doesn’t have to be the same person the entire game"
+    ],
+    "Paul Blart": [
+        "No strong flashlights",
+        "If more than 2 people are playing",
+        "Only 2 equipment per person (including flashlight)"
+    ],
+    "Paul Blart 2 Fat 2 Furious II": [
+        "No strong flashlights",
+        "Only 2 equipment per person (including flashlight)"
+    ],
+    "Paul Blart 3 Peas in a Pod": [
+        "No strong flashlights",
+        "Only 1 equipment per person (including flashlight)"
+    ],
+    "Schools Out, Ghosts In ": [
+        "Highschool on the highest difficulty that is given to you first"
+    ],
+    "“With the Frizz, no way!!”": [
+        "Highschool on the highest difficulty that is given to you first II",
+        "Must stop hunt twice"
+    ],
+    "“Come at me ya pussy bitch!”": [
+        "Must insult and attract the ghost during every hunt.",
+        "Can’t hide, must run",
+        "No jesus boi (Crucifix)"
+    ],
+    "The Security Guard": [
+        "Only one person can have a flashlight, they are the security guard (not including UV)",
+        "All other players can only refer to the guard as “pig”",
+        "Other players can use candles"
+    ],
+    "Found footage": [
+        "1 person must stay in the van the whole time",
+        "Only the person in the van can use the radio",
+        "Every other player needs to wear a Go Pro",
+        "The other players cannot return to the truck after they’ve entered",
+        "Recommend bring equipment to the door of the building"
+    ],
+    "Trust exercise Work in progress": [
+        "One person must stay in the truck and guide the “blind”",
+        "One person in the “blind” group can communicate with the guide",
+        "Everyone else is a mute"
+    ],
+    "Sacrifice! Sacrifice! Sacrifice!": [
+        "Play the game normally until a hunt is happening",
+        "When a hunt is happening one person must sacrifice themself to the ghost to keep the rest of the team alive"
+    ],
+    "The Offering": [
+        "Once the room has been found only one person can stay inside the house and collect evidence",
+        "The remaining players must remain outside until the person inside dies or finds all the evidence"
+    ],
+    "Only Starting Equipment": [
+        "Don’t add ANYTHING before the game, good luck",
+        "If you’re playing with more than 2 people, you may add 1 normal flashlight"
+    ]
+}
 
 function new_challenge() {
-    var num_challenges = challenges.length;
+    var num_challenges = Object.keys(challenges).length
     var random = Math.floor(Math.random() * num_challenges);
 
-    while (past_three.includes(random)) {
-        random = Math.floor(Math.random() * num_challenges);
-    }
-
-    past_three.unshift(random);
-    console.log(past_three);
-    if (past_three.length >= 3) {
-        past_three.pop();
-    }
-
-    return challenges[random];
+    return Object.keys(challenges)[random];
 }
 
 function present_challenge(chal) {
-    var name = Object.keys(chal)[0];
+    var name = chal
+    var desc = challenges[chal]
+
     document.getElementById("strat-name").innerHTML = name;
 
-    var desc = chal[Object.keys(chal)[0]];
-    var desc_element = document.getElementById("strat-desc");
-    if (desc.length > 150) {
-        desc_element.classList.add("justify-text");
+    $("ul").remove()
+
+    var uList = document.createElement("ul")
+    uList.classList.add("center-text")
+
+    for (let i = 0; i < desc.length; i++) {
+        const e = desc[i]
+        let li = document.createElement("li")
+        li.innerHTML = e
+        uList.append(li)
     }
-    else {
-        desc_element.classList.remove("justify-text");
-    }
-    desc_element.innerHTML = desc;
+    
+    $("#roulette").append(uList)
+    $("#strat-intro").remove()
 }
 
 function button_press() {
