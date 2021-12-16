6a8e7fe6f7d25e37b97fbff09a2ef2a9fb82e55b
cranberriez <jakevilevac@gmail.com>
2021-12-16 03:32:34 -0800

    updated challenges, added no repeat system & special player

--- main3.js
+++ main3.js
@@ -1,13 +1,14 @@
-var challenges = {
+const challenges = {
     "EMP ": [
         "No electronic devices",
         "No using lights in the building",
         "No flashlights"
     ],
     "Candles and Glowsticks": [
+		"No flashlights, only candles and glowsticks",
         "Can still use all normal electronics except flashlights (including UV flashlight)"
     ],
-    "Stubborn Cunts ”Anti vax life hax”": [
+    "Stubborn Bitches ”Anti vax life hax”": [
         "No going back to truck until you want to leave",
         "No sanity pills"
     ],
@@ -25,23 +26,17 @@ var challenges = {
     ],
     "Paul Blart": [
         "No strong flashlights",
-        "If more than 2 people are playing",
         "Only 2 equipment per person (including flashlight)"
     ],
-    "Paul Blart 2 Fat 2 Furious II": [
-        "No strong flashlights",
-        "Only 2 equipment per person (including flashlight)"
-    ],
-    "Paul Blart 3 Peas in a Pod": [
+    "Paul Blart (Hard)": [
         "No strong flashlights",
         "Only 1 equipment per person (including flashlight)"
     ],
     "Schools Out, Ghosts In ": [
-        "Highschool on the highest difficulty that is given to you first"
+        "Highschool on Professional"
     ],
     "“With the Frizz, no way!!”": [
-        "Highschool on the highest difficulty that is given to you first II",
-        "Must stop hunt twice"
+        "Highschool on Nightmare"
     ],
     "“Come at me ya pussy bitch!”": [
         "Must insult and attract the ghost during every hunt.",
@@ -53,13 +48,6 @@ var challenges = {
         "All other players can only refer to the guard as “pig”",
         "Other players can use candles"
     ],
-    "Found Footage": [
-        "1 person must stay in the van the whole time",
-        "Only the person in the van can use the radio",
-        "Every other player needs to wear a Go Pro",
-        "The other players cannot return to the truck after they’ve entered",
-        "Recommend bring equipment to the door of the building"
-    ],
     "Trust Exercise": [
         "One person must stay in the truck and guide the “blind”",
         "One person in the “blind” group can communicate with the guide",
@@ -67,34 +55,256 @@ var challenges = {
     ],
     "Sacrifice! Sacrifice! Sacrifice!": [
         "Play the game normally until a hunt is happening",
-        "When a hunt is happening one person must sacrifice themself to the ghost to keep the rest of the team alive"
+        "When a hunt is happening one person must sacrifice themselves to the ghost to keep the rest of the team alive"
     ],
     "The Offering": [
-        "Once the room has been found only one person can stay inside the house and collect evidence",
-        "The remaining players must remain outside until the person inside dies or finds all the evidence"
+        "Once the room has been found one person must remain in the room for the remainder of the game",
+        "Or until the ghost has been figured out",
+        "The remaining players must remain outside the room until the person inside dies or gets all the evidence"
     ],
     "Only Starting Equipment": [
         "Don’t add ANYTHING before the game, good luck",
         "If you’re playing with more than 2 people, you may add 1 normal flashlight"
+    ],
+    "Trust Exercise (Hard)": [
+        "All players wear GoPros.",
+        "No lights and no flashlights (One UV flashlight permitted).",
+		"The special player stays in the van, and guides players through the house over radio, using the map in the van.",
+		"The other players cannot return to the truck after they’ve entered.",
+		"Recommend bring equipment to the door of the building."
+    ],
+    "Radio Silence": [
+        "Once you have entered the building it's as if a radio jammer has come on.",
+        "You can communicate as normal but only locally. Only people OUTSIDE may use radio.",
+        "Best On Larger Maps."
+    ],
+    "Lights Out": [
+        "You may only use candles and lighters.",
+        "No house lights or flashlights.",
+        "One UV flashlight is permitted."
+    ],
+    "Anti Vax": [
+        "No sanity pills allowed.",
+        "Cannot leave until 3 pieces of evidence are confirmed.",
+        "The power cannot be turned on, it causes autism"
+    ],
+    "Apartment Block": [
+        "You all live in a complex and have your own rooms.",
+        "You may bring whatever you want but it is not to be shared, you aren't friendly neighbors.",
+        "Without the use of a thermometer everyone picks their own rooms to investigate and is not allowed to stray from them.",
+        "Best on small maps."
+    ],
+    "A Tall Order": [
+        "You are all short detectives, you must always be crouched.",
+        "No exceptions, even during hunts."
+    ],
+    "Camera Men": [
+        "The shop was all out of video cameras, you could only buy GoPros.",
+        "If you wish to get proof of ghost orbs you must use a player as a cameraman",
+        "Once positioned, the cameraman is not allowed to move no matter what phase the ghost is in.",
+        "The cameraman is released from duty only by their peers."
+    ],
+    "Women's Jeans": [
+        "You can only carry one item.",
+        "This means you may not have space for a flashlight."
+    ],
+    "Budget Cuts": [
+        "Your ghost hunt show has hit a rough patch, your producer has run out of money.",
+        "You can each only bring one item and there are to be no repeated items.",
+        "Union healthcare permits one container of sanity pills each."
+    ],
+    "Spec Ops": [
+        "You can only use the radio after spelling your name out in the NATO phonetic alphabet.",
+        "If you want to talk to another player specifically, you must spell out their name as well.",
+        "Name shortenings allowed (for names with more than 1 syllable)"
+    ],
+    "Dark Mode": [
+        "No lights.",
+        "No exceptions."
+    ],
+    "Loners": [
+        "After first entering the house, no player may enter a room which another player is occupying.",
+        "No exceptions, even in a hunt. The van counts as a room."
+    ],
+    "Haiku": [
+        " ---Speak using haiku---",
+        "-Pattern is five seven five-",
+        " ---Example as shown---",
+        "[A haiku is a poem consisting of three lines, the first line containing five syllables, the second seven and the third contianing five again.]"
+    ],
+    "Lost In Translation": [
+        "Players may need to write this one down.",
+        "Players may only refer to items by the following codewords:",
+        "EMF Reader = The Pager",
+        "Spirit Box = Steven Hawking",
+        "Thermometer = The Coziness Checker",
+        "Ghost Writing Book = The Homework"
+    ],
+    "'W'": [
+        "Players must always move.",
+        "They may never let go of the forwards control.",
+        "No exceptions."
+    ],
+    "Broken Trolley Wheel": [
+        "Players may only ever turn left.",
+        "If a player needs to turn right, they should turn left until facing the correct direction."
+    ],
+    "That Healthy Glow": [
+        "No flashlights.",
+        "All players carry a glowstick."
+    ],
+    "Super Smudge Bros": [
+        "All players enter carrying a thermometer, a lighter, and a smudge stick.",
+        "Upon discovery of the room, all players light up their smudge sticks.",
+        "[Insert your own 420 joke here]"
+    ],
+    "Blair Witch": [
+        "Everyone must have at least one camera type on them at all times (Head cameras do not count).",
+        "Every time a hunting phase starts no matter where in the building you are you must turn to the closest wall and stare at it until the phase it over",
+        "When a player is killed you must enter every room on the map calling out the ghost's name until the body is found. (best played on smaller maps)"
+    ],
+    "Three Point Six Roentgen": [
+        "Only one EMF reader is allowed, and is the only item held by the special player (they may also have a flashlight)",
+        "If the EMF reader displays anything ABOVE level 3, all players must evacuate the building.",
+        "Once all players are outside the search may resume."
+    ],
+    "Freeze!": [
+        "During a hunt, players may not leave the room they are in",
+        "No exceptions."
+    ],
+    "Two Minute Madness": [
+        "All players must wear GoPros.",
+        "Only one player is allowed inside at a time.",
+        "Every two minutes the player MUST leave and swaps with another of their choosing.",
+        "While inside a player cannot return for items until their time is up.",
+        "Best on small maps"
+    ],
+    "Spirit Box Steve": [
+        "The special player is spirit box steve. ",
+        "They can only communicate to other players using the following words",
+        "Kill, Death, Die, Hate, Leave, Hurt, Attack, Catch, Here, Close, Next, Behind, Away, Far, Child, Kid, Adult, Old.",
+        "They may wish to write these down somewhere."
+    ],
+    "The Dark One": [
+        "The special player is the dark one.",
+        "They will turn off the lights of any room they enter, and any players who can see them or are near them must turn off their flashlights.",
+        "If the dark one dies, the player who first spots their corpse becomes the new dark one."
+    ],
+    "That Sinking Feeling": [
+        "All sinks must be turned on before investigation can begin.",
+        "Players must only carry a flashlight until this is done.",
+        "Upon finishing, all sinks must be turned off"
+    ],
+    "Prop Hunt": [
+        "The special player is the saboteur",
+        "They must scatter the default items around the map, hiding them before other players may enter.",
+        "No additional default items may be brought along.",
+        "After this, the saboteur becomes a team player again but will only help if given a found item by another player."
+    ],
+    "Thick As Thieves": [
+        "All players enter the site with a flashlight only (any kind).",
+        "They may not begin searching for the ghost until every collectable electronic device is stolen.",
+        "Stolen devices must be placed in the van.",
+        "Works best on house maps"
+    ],
+    "Door Daddy": [
+        "The special player is the 'Door Daddy'.",
+        "They cannot assist in the search and must try to close the door on every other player, to prevent them from having escape paths.",
+        "They may also hold doors shut against players in a hunt phase.",
+        "If a player says “Door Daddy, Door Daddy, leave me be!” the Door Daddy must open the door."
+    ],
+    "EMP!": [
+        "What happened to the power, did a nuke go off in the upper atmosphere?",
+        "Power is off in the house and use of any electronic devices apart from normal flashlights and a single EMF reader is prohibited."
+    ],
+    "BFFL": [
+        "You buddy up with one other player, making two teams of two.",
+        "You may only communicate to this one other player, and must avoid the other two.",
+        "If your buddy dies, you can no longer assist in the investigation and must weep at their corpse.",
+        "If each pair loses a buddy, they may join together. "
+    ],
+    "Come On In!": [
+        "You sure are a welcoming bunch.",
+        "Once a door is open you are not allowed to close it for any reason.",
+        "Ever! No exceptions..."
+    ],
+    "Yall Need Jesus": [
+        "All players must remain in the same room as a crucifix at all times.",
+        "Two players must carry the crucifixes.",
+        "They may use other items but must hold the crucifix during hunt phases."
+    ],
+    "Supermodel": [
+        "The special player is the supermodel.",
+        "All other players carry cameras.",
+        "Other players may only use their cameras if the supermodel is in frame.",
+        "No exceptions."
+    ],
+    "Nyctophobia": [
+        "You sleep with a nightlight despite your age.",
+        "Every room you enter you must turn the light on and any electronic device in the room.",
+        "if the power goes out at least two players must go together to turn the power back on."
+    ],
+    "Enemy Of My Enemy": [
+        "While alive one of your friends (the special player) is in league with the ghost, their job is to try and sabotage the investigation however they can.",
+        "BUT, during an active phase they aren't allowed to hide, why would they the ghost is their friend?",
+        "If they are killed they must pile as many objects as they can where the ghosts spawns to assist with the investigation."
+    ],
+    "The Armourer": [
+        "The special player is the armourer. The armourer plays as normal (does not just stay in van).",
+        "They are the only people who may remove items from the van, and give only two items at a time to other players.",
+        "If a player wishes to take additional items, they must return their equipment to the armourer.",
+        "Consumed items must also be returned.",
+        "If a player dies, the armourer must collect their dropped gear themselves.",
+        "If the armourer dies, no additional items may be taken."
     ]
 }
 
-function new_challenge() {
-    var num_challenges = Object.keys(challenges).length
-    var random = Math.floor(Math.random() * num_challenges);
+const repeat_wait = 15
+let past_challenges = []
 
-    return Object.keys(challenges)[random];
+function rand_difficulty() {
+    let difficulties = ["Easy","Intermediate","Professional","NIGHTMARE"]
+
+    let len = difficulties.length
+    let random = Math.floor(Math.random() * len)
+
+    return difficulties[random]
 }
 
-function present_challenge(chal) {
-    var name = chal
-    var desc = challenges[chal]
+function rand_player() {
+    let players = ["Red", "Blue", "Green", "Purple"]
 
-    document.getElementById("strat-name").innerHTML = name;
+    let len = players.length
+    let random = Math.floor(Math.random() * len)
+
+    return players[random]
+}
+
+function rand_map() {
+    let maps=[{name:"Willow Street House",size:"tiny"},{name:"Tanglewood Street House",size:"tiny"},{name:"Edgefield Street House",size:"small"},{name:"Ridgeview Street House",size:"small"},{name:"Grafton Farmhouse",size:"small"},{name:"Bleasdale Farmhouse",size:"medium"},{name:"Maple Lodge Campsite",size:"medium"},{name:"High School",size:"large"},{name:"Prison",size:"large"},{name:"Asylum",size:"massive"}]
+    
+    let len = maps.length
+    let random = Math.floor(Math.random() * len)
+
+    return maps[random]
+}
+
+function new_challenge() {
+    let len = Object.keys(challenges).length
+    let random = Math.floor(Math.random() * len)
+
+    return random
+}
+
+function present_challenge(random) {
+    let name = Object.keys(challenges)[random]
+    let desc = challenges[name]
+
+    document.getElementById("strat-name").innerHTML = name
 
     $("ul").remove()
 
-    var uList = document.createElement("ul")
+    let uList = document.createElement("ul")
     uList.classList.add("center-text")
 
     for (let i = 0; i < desc.length; i++) {
@@ -103,11 +313,38 @@ function present_challenge(chal) {
         li.innerHTML = e
         uList.append(li)
     }
+
+    let li = document.createElement("li")
+    li.classList.add("special-player")
+    let player = rand_player()
+    li.classList.add(player.toLowerCase())
+    li.innerHTML = `<br/>Special Player: ${player}`
+    uList.append(li)
     
     $("#roulette").append(uList)
     $("#strat-intro").remove()
 }
 
+function check_repeat(chal) {
+    if (past_challenges.includes(chal)) {
+        return true
+    }
+    else {
+        if (past_challenges.length >= repeat_wait) {
+            past_challenges.pop()
+        }
+
+        past_challenges.unshift(chal)
+        return false
+    }
+}
+
 function button_press() {
-    present_challenge(new_challenge());
+    let loop = true
+    let chal = new_challenge()
+    while (loop) {
+        chal = new_challenge()
+        loop = check_repeat(chal)
+    }
+    present_challenge(chal)
 }
